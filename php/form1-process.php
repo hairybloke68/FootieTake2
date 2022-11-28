c1d4b7f1ad894ce428273f3d5db9bbb2803cee05
office <daveyb_mobile@yahoo.com>
2022-11-28 10:23:25 +0000

    More trying Database

--- php/form1-process.php
+++ php/form1-process.php
@@ -106,26 +106,26 @@ if ($link->connect_error) {
   
 //send data
 
-$sql = "INSERT INTO footieForm1 (fname, sname, d1g1t1, d1g1t2, d2g1t1, d2g1t2, d2g2t1, d2g2g2, d2g3t1, d2g3t2,
-d3g1t1, d3g1t2, d3g2t1, d3g2t2, d3g3t1, d3g3t2, d3g4t1, d3g4t2, 
-d4g1t1, d4g1t2, d4g2t1, d4g2t2, d4g3t1, d4g3t2, d4g4t1, d4g4t2, 
-d5g1t1, d5g1t2, d5g2t1, d5g2t2, d5g3t1, d5g3t2, d5g4t1, d5g4t2, 
-d6g1t1, d6g1t2, d6g2t1, d6g2t2, d6g3t1, d6g3t2, d6g4t1, d6g4t2, 
-d7g1t1, d7g1t2, d7g2t1, d7g2t2, d7g3t1, d7g3t2, d7g4t1, d7g4t2, 
-d8g1t1, d8g1t2, d8g2t1, d8g2t2, d8g3t1, d8g3t2, d8g4t1, d8g4t2,
-d9g1t1, d9g1t2, d9g2t1, d9g2t2, d9g3t1, d9g3t2, d9g4t1, d9g4t2, 
-d10g1t1, d10g1t2, d10g2t1, d10g2t2, d10g3t1, d10g3t2, d10g4t1, d10g4t2, 
-d11g1t1, d11g1t2, d11g2t1, d11g2t2, d11g3t1, d11g3t2, d11g4t1, d11g4t2, 
-d12g1t1, d12g1t2, d12g2t1, d12g2t2, d12g3t1, d12g3t2, d12g4t1, d12g4t2, rank) 
-VALUES ($fname, $sname, $d1g1t1, $d1g1t2, $d2g1t1, $d2g1t2, $d2g2t1, $d2g2g2, $d2g3t1, $d2g3t2,
-$d3g1t1, $d3g1t2, $d3g2t1, $d3g2t2, $d3g3t1, $d3g3t2, $d3g4t1, $d3g4t2, 
-$d4g1t1, $d4g1t2, $d4g2t1, $d4g2t2, $d4g3t1, $d4g3t2, $d4g4t1, $d4g4t2, 
-$d5g1t1, $d5g1t2, $d5g2t1, $d5g2t2, $d5g3t1, $d5g3t2, $d5g4t1, $d5g4t2, 
-$d6g1t1, $d6g1t2, $d6g2t1, $d6g2t2, $d6g3t1, $d6g3t2, $d6g4t1, $d6g4t2, 
-$d7g1t1, $d7g1t2, $d7g2t1, $d7g2t2, $d7g3t1, $d7g3t2, $d7g4t1, $d7g4t2, 
-$d8g1t1, $d8g1t2, $d8g2t1, $d8g2t2, $d8g3t1, $d8g3t2, $d8g4t1, $d8g4t2,
-$d9g1t1, $d9g1t2, $d9g2t1, $d9g2t2, $d9g3t1, $d9g3t2, $d9g4t1, $d9g4t2, 
-$d10g1t1, $d10g1t2, $d10g2t1, $d10g2t2, $d10g3t1, $d10g3t2, $d10g4t1, $d10g4t2, 
-$d11g1t1, $d11g1t2, $d11g2t1, $d11g2t2, $d11g3t1, $d11g3t2, $d11g4t1, $d11g4t2, 
-$d12g1t1, $d12g1t2, $d12g2t1, $d12g2t2, $d12g3t1, $d12g3t2, $d12g4t1, $d12g4t2, 0)";
+$sql = "INSERT INTO footieForm1 ('fname', 'sname', 'd1g1t1', 'd1g1t2', 'd2g1t1', 'd2g1t2', 'd2g2t1', 'd2g2g2', 'd2g3t1', 'd2g3t2',
+'d3g1t1', 'd3g1t2', 'd3g2t1', 'd3g2t2', 'd3g3t1', 'd3g3t2', 'd3g4t1', 'd3g4t2', 
+'d4g1t1', 'd4g1t2', 'd4g2t1', 'd4g2t2', 'd4g3t1', 'd4g3t2', 'd4g4t1', 'd4g4t2', 
+'d5g1t1', 'd5g1t2', 'd5g2t1', 'd5g2t2', 'd5g3t1', 'd5g3t2', 'd5g4t1', 'd5g4t2', 
+'d6g1t1', 'd6g1t2', 'd6g2t1', 'd6g2t2', 'd6g3t1', 'd6g3t2', 'd6g4t1', 'd6g4t2', 
+'d7g1t1', 'd7g1t2', 'd7g2t1', 'd7g2t2', 'd7g3t1', 'd7g3t2', 'd7g4t1', 'd7g4t2', 
+'d8g1t1', 'd8g1t2', 'd8g2t1', 'd8g2t2', 'd8g3t1', 'd8g3t2', 'd8g4t1', 'd8g4t2',
+'d9g1t1', 'd9g1t2', 'd9g2t1', 'd9g2t2', 'd9g3t1', 'd9g3t2', 'd9g4t1', 'd9g4t2', 
+'d10g1t1', 'd10g1t2', 'd10g2t1', 'd10g2t2', 'd10g3t1', 'd10g3t2', 'd10g4t1', 'd10g4t2', 
+'d11g1t1', 'd11g1t2', 'd11g2t1', 'd11g2t2', 'd11g3t1', 'd11g3t2', 'd11g4t1', 'd11g4t2', 
+'d12g1t1', 'd12g1t2', 'd12g2t1', 'd12g2t2', 'd12g3t1', 'd12g3t2', 'd12g4t1', 'd12g4t2', 'rank') 
+VALUES ('$fname', '$sname', '$d1g1t1', '$d1g1t2', '$d2g1t1', '$d2g1t2', '$d2g2t1', '$d2g2g2', '$d2g3t1', '$d2g3t2',
+'$d3g1t1', '$d3g1t2', '$d3g2t1', '$d3g2t2', '$d3g3t1', '$d3g3t2', '$d3g4t1', '$d3g4t2', 
+'$d4g1t1', '$d4g1t2', '$d4g2t1', '$d4g2t2', '$d4g3t1', '$d4g3t2', '$d4g4t1', '$d4g4t2', 
+'$d5g1t1', '$d5g1t2', '$d5g2t1', '$d5g2t2', '$d5g3t1', '$d5g3t2', '$d5g4t1', '$d5g4t2', 
+'$d6g1t1', '$d6g1t2', '$d6g2t1', '$d6g2t2', '$d6g3t1', '$d6g3t2', '$d6g4t1', '$d6g4t2', 
+'$d7g1t1', '$d7g1t2', '$d7g2t1', '$d7g2t2', '$d7g3t1', '$d7g3t2', '$d7g4t1', '$d7g4t2', 
+'$d8g1t1', '$d8g1t2', '$d8g2t1', '$d8g2t2', '$d8g3t1', '$d8g3t2', '$d8g4t1', '$d8g4t2',
+'$d9g1t1', '$d9g1t2', '$d9g2t1', '$d9g2t2', '$d9g3t1', '$d9g3t2', '$d9g4t1', '$d9g4t2', 
+'$d10g1t1', '$d10g1t2', '$d10g2t1', '$d10g2t2', '$d10g3t1', '$d10g3t2', '$d10g4t1', '$d10g4t2', 
+'$d11g1t1', '$d11g1t2', '$d11g2t1', '$d11g2t2', '$d11g3t1', '$d11g3t2', '$d11g4t1', '$d11g4t2', 
+'$d12g1t1', '$d12g1t2', '$d12g2t1', '$d12g2t2', '$d12g3t1', '$d12g3t2', '$d12g4t1', '$d12g4t2', '0')";
 
